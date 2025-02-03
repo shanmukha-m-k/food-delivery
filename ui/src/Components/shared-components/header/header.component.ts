bf56b0ca9a38011b30b77d16dded43a29d770745
Shanmukha M K <56854991+shanmukha-m-k@users.noreply.github.com>
2025-02-03 23:28:24 +0530

    navbar updated
    navbar updated

--- ui/src/Components/shared-components/header/header.component.ts
+++ ui/src/Components/shared-components/header/header.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
-import {  RouterLink,  RouterOutlet } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+import {  RouterLink,  RouterLinkActive,  RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isCollapsed = true;
+  showMenu = false;
+  scrolled = false;
+  isDarkMode = false;
 
+  ngOnInit() {
+    this.checkTheme();
+  }
+
+  @HostListener("window:scroll")
+  onWindowScroll() {
+    this.scrolled = window.scrollY > 50;
+  }
+
+  toggleNavbar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    document.documentElement.setAttribute(
+      "data-theme",
+      this.isDarkMode ? "dark" : "light"
+    );
+    localStorage.setItem("theme", this.isDarkMode ? "dark" : "light");
+  }
+
+  private checkTheme() {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    this.isDarkMode = savedTheme === "dark";
+    document.documentElement.setAttribute("data-theme", savedTheme);
+  }
 }
